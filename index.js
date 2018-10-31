d9f4daaa0ef9ff680f48f8e4ce669a305c26d9c7
Geeta <soragavigeeta@gmail.com>
2018-10-31 23:48:56 +0530

    Added functions

--- index.js
+++ index.js
@@ -15,12 +15,49 @@ app.get('/', function(req, res){
     res.send("I am a chatbot");
 });
 
+
+
 app.get('/webhook/', function(req,res){
     if(req.query['hub.verify_token'] === "umsgbot") {
         res.send(req.query['hub.challenge']);
     }
     res.send("Incorrect token");
 })
+
+app.post('/webhook/',function(req, res){
+
+    let msg_event = req.body.entry[0].messaging;
+    for(let index = 0; index < msg_event.length; index++){
+        let event = msg_event[index];
+        let sender = event.sender.id;
+        if(event.message && event.message.text){
+            let text = event.message.text;
+            sendText(sender, "Text: " + text.subString(0,100));
+        }
+    }
+    res.sendStatus(200);
+
+});
+
+function sendText(sender, text){
+    let msgData = {text: text};
+    request({
+        url: "https://graph.facebook.com/v2.6/me/messages",
+        qs: {access_token, token},
+        method: "POST",
+        json: {
+            receipent: {id: sender},
+            message: msgData
+        }
+    }, function(error, response, body) {
+        if(error) {
+            console.log("Error while sending");
+        } else if(response.body.error){
+            console.log("response body error")
+        }
+
+    })
+}
 
 app.listen(app.get('port'), function(){
     console.log("Running: port");
