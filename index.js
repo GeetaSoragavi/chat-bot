f70e4fe172b1e06d0545466c9033460d6441245e
Geeta <soragavigeeta@gmail.com>
2018-11-01 12:16:48 +0530

    api.ai integrated

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const request = require('request');
 const app = express();
 
 let token = "EAAReVJUEResBAOknqWe7SxjjOv3zxATZCOZCrgxZBLQN7TczfMZCO4ZBfrvwsPQg2lzPgdmf2UAi5LZAHZAQ5H2y7zZCX8LY4LmxpXhRrlCOhcUqh3bcrRhqm0BSP9p7nlxZARNL4xUh3WPblZCLQgKbGTkmkaAnXmXHdVJbqLrLP5twZDZD";
+const apiaiApp = require('apiai')("419773b76c474f8fa3798a6b55989807");
 
 app.set('port',(process.env.PORT || 5000));
 
@@ -29,12 +30,15 @@ app.get('/webhook/', function(req,res){
 app.post('/webhook/',function(req, res){
 
     let msg_event = req.body.entry[0].messaging;
+    let apiai = apiaiApp.textRequest(text, {
+        sessionId: "msg_sessionId"
+    });
     for(let index = 0; index < msg_event.length; index++){
         let event = msg_event[index];
         let sender = event.sender.id;
         if(event.message && event.message.text){
             let text = event.message.text;
-            sendText(sender, "Text: " + text.substring(0,100));
+            sendText(sender, text.substring(0,100));
         }
     }
     res.sendStatus(200);
@@ -42,24 +46,40 @@ app.post('/webhook/',function(req, res){
 });
 
 function sendText(sender, text){
-    let msgData = {text: text};
-    request({
-        url: "https://graph.facebook.com/v2.6/me/messages",
-        qs: {access_token: token},
-        method: "POST",
-        json: {
-            recipient: {id: sender},
-            message: msgData
-        }
-    }, function(error, response, body) {
-        if(error) {
-            console.log("Error while sending");
-        } else if(response.body.error){
-            console.log("response body error");
-            console.log(response.body.error);
-        }
 
-    })
+    let apiai = apiaiApp.textRequest(text, {
+        sessionId: 'msg_sId' 
+    });
+
+    apiai.on('response', function(response) {
+        let aiText = response.result.fulfillment.speech;
+
+        request({
+            url: "https://graph.facebook.com/v2.6/me/messages",
+            qs: {access_token: token},
+            method: "POST",
+            json: {
+                recipient: {id: sender},
+                message: {text: aiText}
+            }
+        }, function(error, response, body) {
+            if(error) {
+                console.log("Error while sending");
+            } else if(response.body.error){
+                console.log("response body error");
+                console.log(response.body.error);
+            }
+    
+        })
+    });
+
+    apiai.on('error', function(error) {
+        console.log(error);
+    });
+
+    apiai.end();
+    
+    
 }
 
 app.listen(app.get('port'), function(){
